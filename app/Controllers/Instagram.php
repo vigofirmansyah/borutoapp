f354e69bbedc20a9a91ac213e0e2572cdfa32aa1
vigofirmansyah <vigofirmansyahputra@gmail.com>
2021-11-25 14:40:03 +0700

    menambahkan getbody

--- app/Controllers/Instagram.php
+++ app/Controllers/Instagram.php
@@ -13,10 +13,10 @@ class Instagram extends BaseController
     public function index()
     {
         $response = $this->client->get('/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username&access_token=IGQVJXb09sWU9Demx6SVBHUVd1V2FpRGhaN3kxTEU3QlR5bHNWMDdqQWhRc3JvQ1V4YUo0Q1hWUVlMQWlhWml1bXRpUmlrUklVeUoxOFg2NmN1SjZAtVDFYakQ2MGhORFhCRGw1aGRB');
-        $datainstagram = json_decode($response->getBody()->getContents(), true);
+
 $data = [
     'title' => 'Instagram',
-    'datainstagram' => $datainstagram,
+    'datainstagram' => $response->getBody(),
 ];
         return view('konten/instagram.php');
     }
