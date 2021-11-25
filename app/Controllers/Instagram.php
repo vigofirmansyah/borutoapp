227dd946420e6c6f83046327f42bb8e6d275be10
vigofirmansyah <vigofirmansyahputra@gmail.com>
2021-11-25 16:15:28 +0700

    mengganti auth jadi query

--- app/Controllers/Instagram.php
+++ app/Controllers/Instagram.php
@@ -9,19 +9,18 @@ class Instagram extends BaseController
         $this->client = \Config\Services::curlrequest();
     }
     public function index()
-    {
+    {    //         ['auth' => ['access_token', 'IGQVJXb09sWU9Demx6SVBHUVd1V2FpRGhaN3kxTEU3QlR5bHNWMDdqQWhRc3JvQ1V4YUo0Q1hWUVlMQWlhWml1bXRpUmlrUklVeUoxOFg2NmN1SjZAtVDFYakQ2MGhORFhCRGw1aGRB'],
+    // ['fields', 'username']]);
         $response = $this->client->request(
-            'GET', 'https://graph.instagram.com/17841445490064128', 
-            [
-    'auth' => ['access_token', 'IGQVJXb09sWU9Demx6SVBHUVd1V2FpRGhaN3kxTEU3QlR5bHNWMDdqQWhRc3JvQ1V4YUo0Q1hWUVlMQWlhWml1bXRpUmlrUklVeUoxOFg2NmN1SjZAtVDFYakQ2MGhORFhCRGw1aGRB'],
-    ['fields', 'username, id']
-]);
+            'GET', 'https://graph.instagram.com/17841445490064128',
+            ['query' => ['access_token' => 'IGQVJXb09sWU9Demx6SVBHUVd1V2FpRGhaN3kxTEU3QlR5bHNWMDdqQWhRc3JvQ1V4YUo0Q1hWUVlMQWlhWml1bXRpUmlrUklVeUoxOFg2NmN1SjZAtVDFYakQ2MGhORFhCRGw1aGRB'],
+            ['fields'=> 'username']] );
         echo $response->getBody();
 $data = [
     'title' => 'Instagram',
     'datainstagram' => $response->getBody(),
 ];
-        return view('konten/instagram.php', $data);
+        return view('konten/instagram.php');
     }
     
 }
