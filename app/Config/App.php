b9b34ab0cba317f43a64a35f24ace4d3f779aeae
vigofirmansyah <vigofirmansyahputra@gmail.com>
2021-11-26 17:58:06 +0700

    menyalakan git ignore

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://borutoaja.com/';
+    public $baseURL = 'https://localhost:8080';
 
     /**
      * --------------------------------------------------------------------------
