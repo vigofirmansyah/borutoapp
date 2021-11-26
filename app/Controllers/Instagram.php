79e1a482b61734d911fdc1b72e49b5c1de0fe06a
vigofirmansyah <vigofirmansyahputra@gmail.com>
2021-11-26 14:16:25 +0700

    masih salah

--- app/Controllers/Instagram.php
+++ app/Controllers/Instagram.php
@@ -11,14 +11,19 @@ class Instagram extends BaseController
                 'base_url'=>'https://graph.instagram.com',
             ]
         );
-        $this->acces_token ='IGQVJXb09sWU9Demx6SVBHUVd1V2FpRGhaN3kxTEU3QlR5bHNWMDdqQWhRc3JvQ1V4YUo0Q1hWUVlMQWlhWml1bXRpUmlrUklVeUoxOFg2NmN1SjZAtVDFYakQ2MGhORFhCRGw1aGRB' ;
+        $this->access_token ='IGQVJXb09sWU9Demx6SVBHUVd1V2FpRGhaN3kxTEU3QlR5bHNWMDdqQWhRc3JvQ1V4YUo0Q1hWUVlMQWlhWml1bXRpUmlrUklVeUoxOFg2NmN1SjZAtVDFYakQ2MGhORFhCRGw1aGRB' ;
         $this->user_id = '17841445490064128';
     }
     public function getUser()
     {     
-         $response_user = $this->client->get($this->user_id, ['query' => ['access_token' => 'IGQVJXb09sWU9Demx6SVBHUVd1V2FpRGhaN3kxTEU3QlR5bHNWMDdqQWhRc3JvQ1V4YUo0Q1hWUVlMQWlhWml1bXRpUmlrUklVeUoxOFg2NmN1SjZAtVDFYakQ2MGhORFhCRGw1aGRB'],
-            ['fields'=> 'account_type,id,media_count,username']]);
-        return $response_user->getBody();    
+        //  $response_user = $this->client->get($this->user_id, ['query' => ['access_token' => $this->access_token],
+        //     ['fields'=> 'account_type,id,media_count,username']]);
+        // $response = $this->client->get($this->user_id, ['query' => ['access_token' => $this->access_token],
+        //     ['fields'=> 'account_type,id,media_count,username']]);
+        // return $response->getBody();   
+        $response = $this->client->request('GET', $this->user_id, ['query' => ['access_token' => $this->access_token],
+            ['fields'=> 'account_type,id,media_count,username']]); 
+        return $response->getBody();
     }
 
 
