17edae6742333a6ad0efcf3b703567c55588d344
vigofirmansyah <vigofirmansyahputra@gmail.com>
2021-11-30 14:37:17 +0700

    mengganti favicon

--- app/Views/konten/index.php
+++ app/Views/konten/index.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+<link rel="icon" href="<?=base_url()?>/favicon.gif" type="image/gif">
     <title>Hello, world!</title>
 </head>
 
--- app/Views/konten/instagram.php
+++ app/Views/konten/instagram.php
@@ -57,7 +57,7 @@
     </div>
 <div class="container">
     <div class="row">
-        <div class="col">
+        <div class="col d-flex justify-content-center">
             <nav aria-label="Page navigation example">
   <ul class="pagination">
     <li class="page-item">
@@ -65,8 +65,6 @@
         <span aria-hidden="true">&laquo;</span>
       </a>
     </li>
-
-       
     <!-- <li class="page-item"><a class="page-link" href="#">1</a></li>
     <li class="page-item"><a class="page-link" href="#">2</a></li>
     <li class="page-item"><a class="page-link" href="#">3</a></li> -->
